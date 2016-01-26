f529db311fbb7bb686c3a3bcb46d8ee284f949ff
Pierre Plantié <pierrepl@ntie.fr>
2016-01-26 23:21:10 +0000

    Java Dequeue

---
+++ domains/Java/Collections/JavaDequeue.java
@@ -0,0 +1,21 @@
+/*
+made for HackerRank Java: Java Dequeue
+Tue 26 Januaray 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deque deque = new ArrayDeque<>();
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+        }
+    }
+}
