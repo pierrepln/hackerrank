a81e64a7d79ec7ac1b7b761fa31fa4bf69df6c4a
Pierre Plantié <pierrepl@ntie.fr>
2016-01-26 23:25:33 +0000

    Java Inheritance 2

---
+++ domains/Java/OOP/Inheritance2.java
@@ -0,0 +1,19 @@
+/*
+made for HackerRank Java: Java Inheritance2
+Tue 26 Januaray 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+class Arithmetic {}
+
+class Adder extends Arithmetic
+{
+    public Adder() {
+        super();
+    }
+    
+    public int add(int a, int b) {
+        return(a+b);
+    }
+}
