8d1566e0a92630cf8c93427bde7f87ec74a5f56c
Pierre Plantié <pierrepl@ntie.fr>
2016-01-27 00:01:09 +0000

    Java Method Overriding

---
+++ domains/Java/OOP/MethodOverriding.java
@@ -0,0 +1,11 @@
+/*
+made for HackerRank Java: Java Method Overriding
+Wed 27 Januaray 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+void get_number_of_team_members()
+{
+   System.out.println("Each team has 11 players in "+get_name());
+}
