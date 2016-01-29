e275d8ece3c8ce3e461d5369a6a6f0ad55c328f2
Pierre Plantié <pierrepl@ntie.fr>
2016-01-30 00:14:24 +0100

    30 Days of Code : Day21

---
+++ contests/30 Days of Code/day21.java
@@ -0,0 +1,15 @@
+/*
+HackerRank Contest : 30 Days of code
+Sat 30 Januaray 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+class Printer
+{
+    public static <E> void printArray(E[] arr) {
+        for (E element : arr) {
+            System.out.println(element);
+        }
+    }
+}
