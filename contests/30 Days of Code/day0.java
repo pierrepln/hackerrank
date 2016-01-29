83fb36c3c7fafa7e127b17f9c7d8152d8c915475
Pierre Plantié <pierrepl@ntie.fr>
2016-01-30 00:10:16 +0100

    30 Days of Code : Day0

---
+++ contests/30 Days of Code/day0.java
@@ -0,0 +1,15 @@
+/*
+HackerRank Contest : 30 Days of code
+Sat 30 Januaray 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+public class HelloWorld {
+
+       public static void main(String[] args) {
+
+            System.out.println("Hello World.\nWelcome to 30 Days of Code.");
+
+       }
+   }
