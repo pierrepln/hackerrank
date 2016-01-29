62af52f19dd32e5edd8320204c358f651e949ce2
Pierre Plantié <pierrepl@ntie.fr>
2016-01-30 00:16:15 +0100

    30 Days of Code : Day28

---
+++ contests/30 Days of Code/day28.java
@@ -0,0 +1,28 @@
+/*
+HackerRank Contest : 30 Days of code
+Sat 30 Januaray 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      while(testCases>0){
+         String pattern = in.nextLine();
+
+         try {
+             Pattern p = Pattern.compile(pattern);
+             System.out.println("Valid");
+         } catch (PatternSyntaxException e) {
+             System.out.println("Invalid");
+         }
+         testCases--;
+      }
+   }
+}
