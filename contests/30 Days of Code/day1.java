8dd163f203a24f0b4857fb405e173493b0c459fc
Pierre Plantié <pierrepl@ntie.fr>
2016-01-30 00:10:31 +0100

    30 Days of Code : Day1

---
+++ contests/30 Days of Code/day1.java
@@ -0,0 +1,26 @@
+/*
+HackerRank Contest : 30 Days of code
+Sat 30 Januaray 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+
+public class DataTypesPractice {
+
+    public static void main(String[] args) {
+      // Write your print statements here!
+      System.out.println("Primitive : double\nPrimitive : char\nPrimitive : boolean\nPrimitive : int\nReferenced : String\nPrimitive : boolean\nPrimitive : double\nPrimitive : char\nReferenced : String");
+
+       /*
+        5.35
+        'a'
+        false
+        100
+        "I am a code monkey"
+        true
+        17.3
+        'c'
+        "derp"*/
+    }
+}
