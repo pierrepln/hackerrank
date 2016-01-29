d748984b03683233411ed1ed7982ec10b4e66ea2
Pierre Plantié <pierrepl@ntie.fr>
2016-01-30 00:16:06 +0100

    30 Days of Code : Day27

---
+++ contests/30 Days of Code/day27.py
@@ -0,0 +1,18 @@
+/*
+HackerRank Contest : 30 Days of code
+Sat 30 Januaray 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+print("5")
+print("3 3")
+print("-1 0 1")
+print("4 3")
+print("-2 -1 0 2")
+print("5 3")
+print("-2 0 1 2 3")
+print("6 3")
+print("-3 -2 -1 0 1 2")
+print("7 3")
+print("-1 0 1 2 3 4 5")
