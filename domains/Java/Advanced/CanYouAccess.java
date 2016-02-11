7a3d7d60db4dcb7c8510f17729ebd3845f62a81d
Pierre Plantié <pierrepl@ntie.fr>
2016-02-11 12:33:35 +0000

    Java : Can you Access ?

---
+++ domains/Java/Advanced/CanYouAccess.java
@@ -0,0 +1,10 @@
+/*
+made for HackerRank Java: Can You Access?
+Thu 11th February 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+Inner in = new Inner();
+o = in.new Private();
+System.out.println(num + " is "+ ((Inner.Private) o).powerof2(num));
