bf78924ce628b666558239fe5a0af88498458f41
Pierre Plantié <pierrepl@ntie.fr>
2016-02-13 16:48:38 +0000

    7 Days of JavaScript/day2.js

---
+++ contests/7 Days of JavaScript/day2.js
@@ -0,0 +1,23 @@
+/*
+HackerRank Contest : 7 Days of JavaScript
+Sat 13th February 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+if (marks > 90)
+    console.log('AA');
+if (marks > 80 && marks <= 90)
+    console.log('AB');
+if (marks > 70 && marks <= 80)
+    console.log('BB');
+if (marks > 60 && marks <= 70)
+    console.log('BC');
+if (marks > 50 && marks <= 60)
+    console.log('CC');
+if (marks > 40 && marks <= 50)
+    console.log('CD');
+if (marks > 30 && marks <= 40)
+    console.log('DD');
+if (marks <= 30)
+    console.log('FF');
