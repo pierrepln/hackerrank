f4653461ad17b52d10caba99b61c541db238c8a5
Pierre Plantié <pierrepl@ntie.fr>
2016-02-14 00:00:13 +0000

    7 Days of JavaScript/day3.js

---
+++ contests/7 Days of JavaScript/day3.js
@@ -0,0 +1,38 @@
+/*
+HackerRank Contest : 7 Days of JavaScript
+Sun 14th February 2016
+
+Made with <3 by Pierre Plantié
+*/
+
+switch (num) {
+    case 9:
+        console.log("NINE");
+        break;
+    case 8:
+        console.log("EIGHT");
+        break;
+    case 7:
+        console.log("SEVEN");
+        break;
+    case 6:
+        console.log("SIX");
+        break;
+    case 5:
+        console.log("FIVE");
+        break;
+    case 4:
+        console.log("FOUR");
+        break;
+    case 3:
+        console.log("THREE");
+        break;
+    case 2:
+        console.log("TWO");
+        break;
+    case 1:
+        console.log("ONE");
+        break;
+    default:
+        console.log("PLEASE TRY AGAIN");
+}
