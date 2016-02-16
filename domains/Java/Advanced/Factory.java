26c42aa14f80fc1a1c06f1915b7eb0e169407337
Pierre Plantié <pierrepl@ntie.fr>
2016-02-16 19:56:26 +0000

    Java Annotations

--- domains/Java/Advanced/Factory.java
+++ domains/Java/Advanced/Factory.java
@@ -1,2 +1,9 @@
+/*
+made for HackerRank Java: Java Factory
+Tue 16th February 2016
+
+Made with <3 by Pierre Plantié
+*/
+
 if (order.equals("pizza") == true) return new Pizza();
 else return new Cake();
