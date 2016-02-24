cf526719ac9ae1b8444da1dc825a8c68323d1dee
Pierre Plantié <pierrepl@ntie.fr>
2016-02-24 00:13:01 +0000

    Ruby/ControlStructures/Each.rb

---
+++ domains/Ruby/ControlStructures/Each.rb
@@ -0,0 +1,12 @@
+=begin
+made for HackerRank Ruby: Ruby Control Structures - Each
+Wed 24th February 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def scoring(array)
+    array.each do |user|
+        user.update_score
+    end
+end
