3e9561703356de97825c0ee3316b3cfd0054192d
Pierre Plantié <pierrepl@ntie.fr>
2016-02-26 00:17:52 +0000

    Ruby/ControlStructures/InfiniteLoop.rb

---
+++ domains/Ruby/ControlStructures/InfiniteLoop.rb
@@ -0,0 +1,10 @@
+=begin
+made for HackerRank Ruby: Ruby Control Structures - Infinite Loop
+Fri 26th February 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+loop do coder.practice
+    break if coder.oh_one?
+end
