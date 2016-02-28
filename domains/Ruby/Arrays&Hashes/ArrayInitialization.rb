afb712904e0de78fe62da3229be2b5b5ca43a0ce
Pierre Plantié <pierrepl@ntie.fr>
2016-02-28 20:20:52 +0000

    Ruby/Arrays&Hashes/ArrayInitialization.rb

---
+++ domains/Ruby/Arrays&Hashes/ArrayInitialization.rb
@@ -0,0 +1,9 @@
+=beginmade for HackerRank Ruby: Ruby Array - Initialization
+Sun 28th February 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+array = Array.new
+array_1 = Array.new(1)
+array_2 = Array.new(2, 10)
