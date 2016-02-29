7a2dff15c4acd7b466ae1e259949892689870bb2
Pierre Plantié <pierrepl@ntie.fr>
2016-02-29 22:00:23 +0000

    Ruby/Arrays&Hashes/ArrayIndexPart2.rb

---
+++ domains/Ruby/Arrays&Hashes/ArrayIndexPart2.rb
@@ -0,0 +1,25 @@
+=beginmade for HackerRank Ruby: Ruby Array - Index, Part 2
+Mon 29th February 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def neg_pos(arr, index)
+    return arr[-index]
+end
+
+def first_element(arr)
+    return arr.first
+end
+
+def last_element(arr)
+    return arr.last
+end
+
+def first_n(arr, n)
+    return arr.take(n)
+end
+
+def drop_n(arr, n)
+    return arr.drop(n)
+end
