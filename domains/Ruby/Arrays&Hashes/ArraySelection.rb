4a66fb4f5d1a701dbfa31c74ff85425af99f12b2
Pierre Plantié <pierrepl@ntie.fr>
2016-03-02 00:47:22 +0000

    Ruby/Arrays&Hashes/ArraySelection.rb

---
+++ domains/Ruby/Arrays&Hashes/ArraySelection.rb
@@ -0,0 +1,21 @@
+=beginmade for HackerRank Ruby: Ruby Array - Selection
+Wed 2nd February 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def select_arr(arr)
+    return arr.select! {|a| !(a % 2)}
+end
+
+def reject_arr(arr)
+    return arr.reject {|a| a % 3}
+end
+
+def delete_arr(arr)
+    return arr.delete_if {|a| a < 0}
+end
+
+def keep_arr(arr)
+    return arr.keep_if {|a| a >= 0}
+end 
