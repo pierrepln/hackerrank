01a8029bc993abe579724f5550ad2d3875c0fbba
Pierre Plantié <pierrepl@ntie.fr>
2016-03-03 22:46:26 +0000

    HashInitialization.rb

---
+++ domains/Ruby/Arrays&Hashes/HashInitialization.rb
@@ -0,0 +1,9 @@
+=beginmade for HackerRank Ruby: Ruby Hash - Initialization
+Thu 3rd March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+empty_hash = Hash.new
+default_hash = Hash.new(1)
+hackerrank = {"simmy" => 100, "vivmbbs" => 200}
