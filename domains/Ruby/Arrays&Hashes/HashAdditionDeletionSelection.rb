52dd8c4769ad36717e61fd545b292bf553060f7f
Pierre Plantié <pierrepl@ntie.fr>
2016-03-04 00:06:23 +0000

    Ruby/Arrays&Hashes/HashAdditionDeletionSelection.rb

---
+++ domains/Ruby/Arrays&Hashes/HashAdditionDeletionSelection.rb
@@ -0,0 +1,9 @@
+=beginmade for HackerRank Ruby: Ruby Hash - Initialization
+Fri 4th March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+hackerrank.store(543121, 100)
+hackerrank.keep_if {|k ,v| k.is_a?(Integer) == true}
+hackerrank.delete_if{|k, v| k % 2 == 0}
