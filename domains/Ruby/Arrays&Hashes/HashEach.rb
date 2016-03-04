572b4185cd763e575b74103b073deb2eeb15d9e3
Pierre Plantié <pierrepl@ntie.fr>
2016-03-04 00:05:05 +0000

    Ruby/Arrays&Hashes/HashEach.rb

---
+++ domains/Ruby/Arrays&Hashes/HashEach.rb
@@ -0,0 +1,12 @@
+=beginmade for HackerRank Ruby: Ruby Hash - Each
+Fri 4th March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def iter_hash(hash)
+    hash.each do |k ,v|
+        puts k
+        puts v
+    end
+end
