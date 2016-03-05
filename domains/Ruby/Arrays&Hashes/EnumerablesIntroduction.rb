39ed59fb370b1a59a95df70bc3b5b0d06ebca8e2
Pierre Plantié <pierrepl@ntie.fr>
2016-03-05 00:06:34 +0000

    Ruby/Arrays&Hashes/EnumerablesIntroduction.rb

---
+++ domains/Ruby/Arrays&Hashes/EnumerablesIntroduction.rb
@@ -0,0 +1,13 @@
+=beginmade for HackerRank Ruby: Ruby Enumerables - Introduction
+Sat 5th March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def iterate_colors(colors)
+    a = []
+    colors.each do |x|
+        a.push(x)
+    end
+    return a
+end
