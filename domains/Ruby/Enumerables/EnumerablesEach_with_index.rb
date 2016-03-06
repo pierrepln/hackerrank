657209c0d5bf27752f7cade7981ccce07282828c
Pierre Plantié <pierrepl@ntie.fr>
2016-03-06 17:53:14 +0000

    Ruby/Arrays&Hashes/EnumerablesIntroduction.rb

---
+++ domains/Ruby/Enumerables/EnumerablesEach_with_index.rb
@@ -0,0 +1,15 @@
+=beginmade for HackerRank Ruby: Ruby Enumerables - each_with_index
+Sun 6th March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def skip_animals(animals, skip)
+    output = []
+    animals.each_with_index do |item, index|
+        if (index > skip - 1)
+            output.push("#{index}:#{item}")
+        end
+    end
+    return output
+end
