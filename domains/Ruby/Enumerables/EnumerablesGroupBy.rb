e503ecb5cd925aaf61c958570243a291279db1d9
Pierre Plantié <pierrepl@ntie.fr>
2016-03-07 14:49:20 +0000

    Ruby/Enumerables/EnumerablesGroupBy.rb

---
+++ domains/Ruby/Enumerables/EnumerablesGroupBy.rb
@@ -0,0 +1,9 @@
+=beginmade for HackerRank Ruby: Ruby Enumerables - group_by
+Mon 7th March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def group_by_marks(marks, n)
+    marks.group_by { |k,v| v > n ? "Passed" : "Failed" }
+end
