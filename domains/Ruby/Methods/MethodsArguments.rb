582e3219f89f21c805cb6545fba614b948df390f
Pierre Plantié <pierrepl@ntie.fr>
2016-03-13 00:00:44 +0000

    Ruby/Methods/MethodsArguments.rb

---
+++ domains/Ruby/Methods/MethodsArguments.rb
@@ -0,0 +1,10 @@
+=begin
+made for HackerRank Ruby: Ruby Methods - Arguments
+Sun 13th March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def take(array, index = 1)
+  return array.drop(index)
+end
