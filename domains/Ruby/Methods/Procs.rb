6ff46ef6f939d4befd6295c3270a050acf46c0d1
Pierre Plantié <pierrepl@ntie.fr>
2016-03-17 09:54:57 +0000

    Ruby/Methods/Procs.rb

---
+++ domains/Ruby/Methods/Procs.rb
@@ -0,0 +1,18 @@
+=begin
+made for HackerRank Ruby: Procs
+Thu 17th March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def square_of_sum (my_array, proc_square, proc_sum)
+    sum = proc_sum.call(my_array)
+    proc_square.call(sum)
+end
+
+proc_square_number = lambda{|n| n**2}
+proc_sum_array = lambda{|a| a.reduce(0,:+)}
+
+my_array = gets.split().map(&:to_i)
+
+puts square_of_sum(my_array, proc_square_number, proc_sum_array)
