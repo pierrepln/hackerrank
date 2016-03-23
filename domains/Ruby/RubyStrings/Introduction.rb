2d131a8552708b91762decc42aa41400c3db568a
Pierre Plantié <pierrepl@ntie.fr>
2016-03-23 21:08:17 +0000

    Ruby/RubyStrings/Introduction.rb

---
+++ domains/Ruby/RubyStrings/Introduction.rb
@@ -0,0 +1,20 @@
+=begin
+made for HackerRank Ruby: Strings Introduction
+Wed 23th March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def single_quote
+  return 'Hello World and others!'
+end
+
+def double_quote
+  return "Hello World and others!"
+end
+
+def here_doc
+  document = <<-LIMIT
+  Hello World and others!
+  LIMIT
+end
