b8befe03172daac055335ede84d3d5ecd047fb55
Pierre Plantié <pierrepl@ntie.fr>
2016-03-31 20:52:04 +0100

    Algorithms/Warmup/SolveMeFirst.rb

---
+++ domains/Algorithms/Warmup/SolveMeFirst.rb
@@ -0,0 +1,15 @@
+=begin
+made for HackerRank Algorithms: Warmup - Solve Me First
+Thu 31th March 2016
+
+Made with <3 by Pierre Plantié
+=end
+
+def solveMeFirst (a, b)
+    return a + b
+end
+
+val1 = gets.to_i
+val2 = gets.to_i
+sum = solveMeFirst(val1,val2)
+print (sum)
