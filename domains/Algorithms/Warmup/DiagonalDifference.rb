90068339bbe7fa9bdd6d4d5496cdac79216dc2ed
Pierre Plantié <pierrepl@ntie.fr>
2016-04-05 11:02:09 +0100

    Algorithms/Warmup/DiagonalDifference.rb

--- domains/Algorithms/Warmup/DiagonalDifference.rb
+++ domains/Algorithms/Warmup/DiagonalDifference.rb
@@ -6,3 +6,18 @@ Made with <3 by Pierre Plantié
 =end
 
 #!/bin/ruby
+
+n = gets.strip.to_i
+a = Array.new(n)
+for a_i in (0..n-1)
+    a_t = gets.strip
+    a[a_i] = a_t.split(' ').map(&:to_i)
+end
+left_right=0
+right_left=0
+a.each_with_index do |array, index|
+    left_right += array[index]
+    right_left += array[-index-1]
+end
+v = right_left - left_right
+puts v.abs
